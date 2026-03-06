37c86cdcf9031f8419d13d4a9935c56b046be57e
Vipul Mehra <vipulmehra002@gmail.com>
2026-03-06 12:14:21 +0530

    update the code

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -380,7 +380,7 @@ onDeleteUser(user: any): void {
 }
 
 
-// Update user details emitted from UsersTab modals.
+// Update user details emitted from UsersTab modal.
 updateUserFromModal(updatedData: { username: string; email: string; firstName: string; lastName: string }): void {
   if (!updatedData?.username) {
     alert('No user selected for editing');
